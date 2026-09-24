77f290b2a59a8f317ae9166025b844a8aaef879a
coffinyare <mohayare512@gmail.com>
2026-09-24 20:33:15 +0300

    -OAuth-Implement-Sign-In-with-Google

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+
+
+function App(){
+const[expenses, setExpenses]= useState([])
+const [formData, setFormData] = useState({
+  foodName: '',
+  amount: '',
+  date: ''
+});
+
+function  handleChange(event){
+  const{name,  value}=event.target;
+  setFormData((prevFormData)=>{
+    return{...prevFormData, [name]: value}
+  })
+
+}
+function handleClick(event) {
+  event.preventDefault();
+
+  setExpenses((prevExpenses) => {
+    return [...prevExpenses, formData];
+  });
+
+  setFormData({
+    foodName: '',
+    amount: '',
+    date: ''
+  });
+}
+
+
+return(
+  <>
+<form className="expense-form">
+    <input className="input"
+    onChange={handleChange} 
+    name="foodName" 
+    type="text" 
+    value={formData.foodName}
+    placeholder="enter the food name"
+   />
+  <input className="input"
+   onChange={handleChange} 
+   name="amount" 
+   type="number" 
+   value={formData.amount}
+   placeholder="enter the amount"
+  />
+  <input className="input"
+   onChange={handleChange} 
+   name="date" 
+   type="date" 
+   placeholder="enter the date"
+   value={formData.date}
+
+  />
+  <button className="button" onClick={handleClick} type="submit"> 
+    add expense
+    </button>
+
+
+
+  
+</form>
+{expenses.map((expense, index)=>{
+    return(
+        
+     <div className="expense" key={index}>
+      <p> food: {expense.foodName}</p>
+      <p> amount: {expense.amount}</p>
+      <p> date: {expense.date}</p>
+    </div>
+    )
+  })
+}
+</>
+)
+
+}
+
+
+
+
+
+
+export default App;
